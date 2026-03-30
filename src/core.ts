3720571968ca71086911fa69f445836e8b720b8c
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-03-30 15:26:04 +0800

    Create core.ts
    Signed-off-by: Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>

---
+++ src/core.ts
@@ -0,0 +1,14 @@
+/*
+  (c) ItzAaronPlayzYT - The Apache 2.0 License
+*/
+export default function PRINT(info) {
+  // Will Return Info
+  return {
+    info;
+  }
+}
+export default function REM(text) {
+  return {
+    // REM Is A Placeholder
+  }
+}
