9540aec38d3a20efcc601143af8a032ede8e6447
Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>
2026-03-30 17:26:08 +0800

    Create setDefiner.ts
    Signed-off-by: Itz-Aaron-Playz-666 <bw210077@wisdom.edu.hk>

---
+++ src/BASICFuctions/setDefiner.ts
@@ -0,0 +1,7 @@
+export default const keywords = new Set([
+  "PRINT", "LET", "INPUT", "IF", "THEN", "ELSE", "FOR", "NEXT",
+  "WHILE", "WEND", "GOTO", "GOSUB", "RETURN", "END",
+  "DIM", "REM", "STOP", "ON", "DEF", "READ", "DATA", "RESTORE",
+  "CHR$", "MID$", "LEFT$", "RIGHT$", "LEN", "ASC", "VAL", "STR$",
+  "ABS", "SIN", "COS", "TAN", "LOG", "EXP", "SQR", "RND"
+]);
